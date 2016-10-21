145421e02dcda995ddeca4cbffc151a76db3d984
Mohsin Farooqui <mohsin.farooqui4@gmail.com>
2016-10-21 13:03:39 +0530

    initial commit

--- MFEvenodd/main.m
+++ MFEvenodd/main.m
@@ -6,12 +6,23 @@
 //  Copyright © 2016 Mohsin Farooqui. All rights reserved.
 //
 
+void evenodd(int);
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int a;
+        printf("Enter a number\n");
+        scanf("%d",&a);
+        evenodd(a);
     }
     return 0;
+}
+
+void evenodd(int x)
+{
+   if(x%2==0)
+       printf("The number is even\n");
+    else
+        printf("The number is odd\n");
 }
